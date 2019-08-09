4752288b218001130280e08249fef0f130249c7b
youshy <devilyoushy@gmail.com>
2019-08-09 09:27:02 +0100

    Now it works as it supposed to work
    Media is returned via json, not via some weird magic tricks

--- main.go
+++ main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
-	"time"
 )
 
 const wikiAddress = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
 
-const address = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow"
-
-// Taken from amazing sadbox/mediawiki
+// Taken from amazing sadbox/mediawiki (with some changes ofc)
 // Response is a struct used for unmarshaling the MediaWiki JSON response.
 type Response struct {
 	Query struct {
 		// The JSON response for this part of the struct is dumb.
 		// It will return something like { '23': { 'pageid': 23 ...
-		//
 		// As a workaround you can use PageSlice which will create
 		// a list of pages from the map.
 		Pages map[string]Page
@@ -39,37 +35,13 @@ func (r *Response) PageSlice() []Page {
 }
 
 // A Page represents a MediaWiki page and its metadata.
+// This is a modified struct to work with the return type from Wiki API
+// Fields left for readability
 type Page struct {
-	Pageid    int
-	Ns        int
-	Title     string
-	Touched   string
-	Lastrevid int
-	// Mediawiki will return '' for zero, this makes me sad.
-	// If for some reason you need this value you'll have to
-	// do some type assertion sillyness.
-	Counter   interface{}
-	Length    int
-	Edittoken string
-	Revisions []struct {
-		Revid         int       `json:"revid"`
-		Parentid      int       `json:"parentid"`
-		Minor         string    `json:"minor"`
-		User          string    `json:"user"`
-		Userid        int       `json:"userid"`
-		Timestamp     time.Time `json:"timestamp"`
-		Size          int       `json:"size"`
-		Sha1          string    `json:"sha1"`
-		ContentModel  string    `json:"contentmodel"`
-		Comment       string    `json:"comment"`
-		ParsedComment string    `json:"parsedcomment"`
-		ContentFormat string    `json:"contentformat"`
-		Body          string    `json:"*"` // Take note, MediaWiki literally returns { '*':
-	}
-	Imageinfo []struct {
-		Url            string
-		Descriptionurl string
-	}
+	//	Pageid  int
+	//	Ns      int
+	//  Title   string
+	Extract string
 }
 
 func main() {
@@ -96,22 +68,12 @@ func main() {
 		log.Fatal(readErr)
 	}
 
-	bodyToString := string(body)
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pl := response.PageSlice()
 
-	re := regexp.MustCompile(`\b(\w*extract\w*)\b`)
-
-	result := re.Split(bodyToString, -1)
-
-	fmt.Println(result[1])
-
-	// fmt.Println(strings.TrimLeft(bodyToString, "extract"))
-
-	// this will return the JSON
-	// needs a lot of work
-	// var response Response
-	// err = json.Unmarshal(body, &response)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// pl := response.PageSlice()
+	fmt.Println(pl)
 }
